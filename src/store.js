13d121b88e899d9382bfcf35a16de02cd129d405
ali27-dev <khanali3229@gmail.com>
2026-02-07 15:53:31 +0500

    Working with redux store,I'm create initailState, reducer function and test it.

--- src/store.js
+++ src/store.js
@@ -1,15 +1,51 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { createStore } from "redux";
+
 const initailState = {
-  blance: 0,
+  balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
 function reducer(state = initailState, action) {
   switch (action.type) {
-    case value:
-      break;
+    case "account/balance":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestloan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payloan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
 
     default:
       break;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/balance", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "account/withdraw", payload: 30 });
+console.log(store.getState());
+store.dispatch({
+  type: "account/requestloan",
+  payload: { amount: 1000, purpose: "Buy car" },
+});
+console.log(store.getState());
+store.dispatch({
+  type: "account/payloan",
+});
+console.log(store.getState());
