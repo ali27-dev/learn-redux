bd7de4e05ae8103b6c1e697dea334011eff3c523
ali27-dev <khanali3229@gmail.com>
2026-02-07 15:03:59 +0500

    learning redux,First of all i'm import the bank account project and run it.

---
+++ src/store.js
@@ -0,0 +1,15 @@
+const initailState = {
+  blance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initailState, action) {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+}
