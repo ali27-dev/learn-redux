0c84f8d19ab08c253d987c5c972e3a3e6a45536a
ali27-dev <khanali3229@gmail.com>
2026-02-11 23:04:41 +0500

    Creating the CustomerSlice,By implementing the RTk.

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,11 +1,42 @@
 // import store from "../../store";
 
-const initailStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+import reducer from "../accounts/accountSlice";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
-
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateCustomer(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+console.log(customerSlice);
+export const { createCustomer, updateCustomer } = customerSlice.actions;
+export default customerSlice.reducer;
+/*
 export default function customerReducer(state = initailStateCustomer, action) {
   switch (action.type) {
     case "customer/createCustomer":
@@ -35,7 +66,7 @@ export function createCustomer(fullName, nationalID) {
 export function updateCustomer(fullName) {
   return { type: "customer/updateCustomer", payload: fullName };
 }
-
+*/
 // store.dispatch(createCustomer("Abid", "3229"));
 // store.dispatch(updateCustomer("Abid Ali"));
 // console.log(store.getState());
